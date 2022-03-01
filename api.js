7ff08a5f1b321c76400ef123754779177cc257d4
imrannaazir <imrannaaziremon@gmail.com>
2022-03-02 01:39:41 +0600

    details successfully displaied but cannot remove previous details

--- api.js
+++ api.js
@@ -1,4 +1,3 @@
-console.log('where is the problem');
 const getButton = () => {
     const searchField = document.getElementById('search-field')
     const searchValue = searchField.value
@@ -13,7 +12,6 @@ const getButton = () => {
 const displayPhones = phones => {
     for (const brand of phones) {
         const cards = document.createElement('div')
-        console.log(brand.phone_name);
         cards.innerHTML = `
         <div class="col shadow ">
         <div class="card h-100">
@@ -23,13 +21,61 @@ const displayPhones = phones => {
                 <h5 class="card-title text-center">Brand: ${brand.brand}</h5>
 
             </div>
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="updateDetails('${brand.slug}')">
          Details</button>
         </div>
     </div>`
 
         const cardsContainer = document.getElementById('cards-container')
         cardsContainer.appendChild(cards)
-        console.log(cards);
+
     }
+}
+const updateDetails = id => {
+    const detailsUrl = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(detailsUrl)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
+}
+const displayDetails = details => {
+    const detailsDiv = document.createElement('div')
+    console.log();
+    detailsDiv.innerHTML = `
+    <div class="container">
+    <div class="row">
+        <div class="col-5 text-center">
+            <img class="mt-3 mb-3" width="250px"
+                src="${details.image}" alt="">
+        </div>
+        <div class="col-7">
+            <div class="">
+                <h1>${details.name}</h1>
+                <h3>Brand:${details.brand}</h3>
+                <p>Released 2021, September 24</p>
+                <h3>Features:</h3>
+                <p>>Storage: ${details.storage}</p>
+                <p>>Display:${details.displaySize}</p>
+                <p>>Chipset: ${details.chipSet}</p>
+                <p>>Memory:${details.memory}<p>
+                <h3>sensors:</h3>
+                <p>${details.sensors}</p>
+            </div>
+        </div>
+    </div>
+
+</div>`
+    const modalBody = document.getElementById('modal-body')
+    modalBody.appendChild(detailsDiv)
+
+
+
+}
+
+
+const clearModal = () => {
+    const modalBody = document.createElement('modal-body').innerHTML = ''
+
+
+
+
 }
